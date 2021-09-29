d316cde03b69d82c8afa792cac707e37e7fa939f
Mert Koksal <mert.koksal@hamurlabs.com>
2021-09-29 17:39:19 +0300

    Initial Commit

---
+++ Shared/BusStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusStationApp.swift
+//  Shared
+//
+//  Created by Mert Köksal on 29.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct BusStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
